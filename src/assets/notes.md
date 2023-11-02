7359be2cf9175143772cfb3b0339057adbfabc15
tony tolver <andrademaresantonio00@gmail.com>
2023-11-02 00:40:36 -0600

    ADD estilos globales de la aplicación

---
+++ src/assets/notes.md
@@ -0,0 +1,6 @@
+# forma de crear el proyecto
+npm create vite@latest admin-gastos -- --template vue
+
+# si usamos scss
+instalamos con npm i para que se instale en el package.json
+
---
+++ src/components/Filtro.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum voluptates perspiciatis veniam eius aut suscipit ut obcaecati cum ipsam, eos consequuntur corrupti provident! Quo, excepturi. Aliquid iure officiis illum quidem.
+</template>
+
+
+<style scoped>
+
+</style>
