277b1c98edb818fded7ea6fb93061d994cff5f43
tony tolver <andrademaresantonio00@gmail.com>
2023-11-02 01:04:23 -0600

    ADD diseño del formulario

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import Filtro from './components/Filtro.vue'
+  import Presupuesto from './components/Presupuesto.vue'
 
 </script>
 
@@ -7,6 +7,13 @@
   <div>
     <header>
       <h1>Planificador de Gastos</h1>
+
+      <div class="contenedor contenedor-header sombra">
+        <Presupuesto
+        
+        />
+      </div>
+
     </header>     
   </div>
 </template>
@@ -48,6 +55,22 @@
     margin: 0;
     color: var(--blanco);
     text-align: center;
+  }
+  .contenedor{
+    width: 90%;
+    max-width: 80rem;
+    margin: 0 auto;
+  }
+  .contenedor-header{
+    margin-top: -5rem;
+    transform: translateY(5rem);
+    padding: 5rem;
+  }
+  .sombra{
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+    background-color: var(--blanco);
+    border-radius:1.2rem;
+    padding: 5rem;
   }
 
 </style>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+</script>
+
+<template>
+   <form
+         class="presupuesto"
+   >
+      <div class="campo">
+         <label for="">Definir Presupuesto</label>
+         <input 
+               id="nuevo-presupuesto"
+               class="nuevo-presuuesto"
+               placeholder="Añade tu presupuesto"
+               type="number"
+         />
+      </div>
+
+      <input type="submit" value="Definir Presupuesto">
+
+   </form>
+</template>
+
+<style scoped>
+   .presupuesto{
+      width: 100%;
+   }
+   .campo{
+      display: grid;
+      gap: 2rem;
+   }
+   .presupuesto label{
+      font-size: 2.4rem;
+      font-weight: 500;
+      color: var(--azul);
+      text-align: center;
+   }
+   .presupuesto input[type="number"]{
+      background-color: var(--gris-claro);
+      border-radius: 1rem;
+      padding: 1rem;
+      border: none;
+      font-size: 2.2rem;
+      text-align: center;
+   }
+   .presupuesto input[type="submit"]{
+      background-color: var(--azul);
+      border: none;
+      padding: 1rem;
+      text-align: center;
+      font-size: 2rem;
+      margin-top: 2rem;
+      color: var(--blanco);
+      font-weight: 900;
+      width: 100%;
+      transition: background-color 300ms ease;
+   }
+   .presupuesto input[type="submit"]:hover{
+      cursor: pointer;
+      background-color: #1048A4;
+   }
+
+
+</style>
