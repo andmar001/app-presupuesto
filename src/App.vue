7c2c8d1d38bee95a596f3efeb5efe196a742e479
tony tolver <andrademaresantonio00@gmail.com>
2023-11-02 23:47:51 -0600

    ADD estructura del modal

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
   
   import Presupuesto from './components/Presupuesto.vue'
   import ControlPresupuesto from './components/ControlPresupuesto.vue'
+  import Modal from './components/Modal.vue'
 
   import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
+  const modal = reactive({
+    mostrar: false,
+    animar: false
+  })
 
   const presupuesto= ref(0)
   const disponible = ref(0)
@@ -13,6 +19,12 @@
     presupuesto.value = cantidad
     disponible.value = cantidad
   } 
+
+  const mostrarModal = () =>{
+    modal.mostrar = true
+    modal.animar = true
+  }
+
 </script>
 
 <template>
@@ -41,8 +53,14 @@
         <img 
             :src="iconoNuevoGasto"
             alt="Icono Nuevo Gasto"
+            @click="mostrarModal"
         >
       </div>
+
+      <Modal
+          v-if="modal.mostrar"
+      />
+    
     </main>
     
   </div>
---
+++ src/components/Modal.vue
@@ -0,0 +1,14 @@
+<script setup>
+  
+</script>
+
+<template>
+   <div>
+         <h1>Desde el modal</h1>
+   </div>
+</template>
+
+
+<style scoped>
+
+</style>
