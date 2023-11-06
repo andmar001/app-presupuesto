55aa1c84fcf7cd99a5e28c443f9db01889f3aac6
tony tolver <andrademaresantonio00@gmail.com>
2023-11-06 17:14:49 -0600

    ADD diseño de modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -13,6 +13,51 @@
             @click="emit('ocultar-modal')"
          />
       </div>
+      <div class="contenedor">
+         <form
+            class="nuevo-gasto"
+         >
+            <legend>Añadir Gasto</legend>
+            
+            <div class="campo">
+               <label for="nombre">Nombre Gasto: </label>
+               <input
+                  type="text"
+                  id="nombre"
+                  placeholder="Añade el nombre del gasto"
+               />
+            </div>
+            <div class="campo">
+               <label for="cantidad">Cantidad: </label>
+               <input
+                  type="text"
+                  id="cantidad"
+                  placeholder="Añade la cantidad del gasto, ej. 300"
+               />
+            </div>
+            <div class="campo">
+               <label for="categoria">Categoria:</label>
+               <select 
+                  id="categoria"
+               >
+                  <option value="">-- Seleccione --</option>
+                  <option value="ahorro">Ahorro</option>
+                  <option value="comida">Comida</option>
+                  <option value="casa">Casa</option>
+                  <option value="gastos">Gastos Varios</option>
+                  <option value="ocio">Ocio</option>
+                  <option value="salud">Salud</option>
+                  <option value="suscripciones">Suscripciones</option>
+               </select>
+            </div>
+
+            <input
+               type="submit"
+               class="boton"
+               value="Añadir Gasto"
+            />
+         </form>
+      </div>
    </div>
 </template>
 
@@ -33,6 +78,40 @@
    }
    .cerrar-modal img{
       width: 3rem;
+      cursor: pointer;
+   }
+   .nuevo-gasto{
+      margin: 10rem auto 0 auto;
+      display: grid;
+      gap: 2rem;
+   }
+   .nuevo-gasto legend{
+      font-size: 3rem;
+      color: var(--blanco);
+      text-align: center;
+      font-weight: 700;
+   }
+   .campo{
+      display: grid;
+      gap: 2rem;
+   }
+   .nuevo-gasto input,
+   .nuevo-gasto select{
+      background-color: var(--gris-claro);
+      border-radius:1rem;
+      padding: 1rem;
+      border:none;
+      font-size: 2.2rem;
+   }
+   .nuevo-gasto label{
+      color: var(--blanco);
+      font-size: 3rem;
+   }
+
+   .nuevo-gasto input[type="submit"]{
+      background-color: var(--azul);
+      color: var(--blanco);
+      font-weight: 700;
       cursor: pointer;
    }
 </style>
