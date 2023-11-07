f6d55c9bac449fe78ab2d3b53763276e25b53e7b
tony tolver <andrademaresantonio00@gmail.com>
2023-11-07 16:02:06 -0600

    ADD componente para filtrar gastos por categoría

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   import ControlPresupuesto from './components/ControlPresupuesto.vue'
   import Modal from './components/Modal.vue'
   import Gasto from './components/Gasto.vue'
+  import Filtros from './components/Filtros.vue'
 
   import { generarId } from './helpers'
 
@@ -18,6 +19,7 @@
   const presupuesto= ref(0)
   const disponible = ref(0)
   const gastado = ref(0)
+  const filtro = ref('')
 
   const gasto = reactive({
     nombre: '',
@@ -134,6 +136,10 @@
     </header>
     
     <main v-if="presupuesto > 0">
+
+      <Filtros
+        v-model:filtro="filtro"
+      />
 
       <div class="listado-gastos contenedor">
         <h2>{{ gastos.length > 0 ? 'Gastos':'No hay gastos'}}</h2>
